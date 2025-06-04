1f933229988c01c41ab593b8b58f8f68858fee04
Victor Carnielli <garevictor@hotmail.com>
2025-06-04 20:34:33 +0100

    Initial migration

--- src/BestiaryArenaCracker.Repository/Migrations/20250603211504_InitialMigration.cs
+++ src/BestiaryArenaCracker.Repository/Migrations/20250603211504_InitialMigration.cs
@@ -10,23 +10,76 @@ namespace BestiaryArenaCracker.Repository.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            // Composition table
             migrationBuilder.CreateTable(
-                name: "Rooms",
+                name: "Composition",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Id = table.Column<int>(type: "int", nullable: false).Annotation("SqlServer:Identity", "1, 1"),
+                    CompositionHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RoomId = table.Column<string>(type: "varchar(50)", nullable: false),
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                    table.PrimaryKey("PK_Composition", x => x.Id);
+                });
+
+            // Composition Monsters
+            migrationBuilder.CreateTable(
+                name: "CompositionMonsters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false).Annotation("SqlServer:Identity", "1, 1"),
+                    CompositionId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Hitpoints = table.Column<int>(type: "int", nullable: false),
+                    Attack = table.Column<int>(type: "int", nullable: false),
+                    AbilityPower = table.Column<int>(type: "int", nullable: false),
+                    Armor = table.Column<int>(type: "int", nullable: false),
+                    MagicResistance = table.Column<int>(type: "int", nullable: false),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    TileLocation = table.Column<int>(type: "int", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompositionMonsters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CompositionMonsters_Composition_CompositionId",
+                        column: x => x.CompositionId,
+                        principalTable: "Composition",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            // Composition result
+            migrationBuilder.CreateTable(
+                name: "CompositionResult",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false).Annotation("SqlServer:Identity", "1, 1"),
+                    CompositionId = table.Column<int>(type: "int", nullable: false),
+                    Ticks = table.Column<int>(type: "int", nullable: false),
+                    Points = table.Column<int>(type: "int", nullable: false),
+                    Grade = table.Column<string>(type: "varchar(10)", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompositionResult", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CompositionResult_Composition_CompositionId",
+                        column: x => x.CompositionId,
+                        principalTable: "Composition",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
                 });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropTable("Composition");
+            migrationBuilder.DropTable("CompositionMonsters");
+            migrationBuilder.DropTable("CompositionResult");
         }
     }
 }
