90f7c1e4a65efbb0f0572c3d6ae47835451cc7b1
Victor Carnielli <garevictor@hotmail.com>
2025-06-05 21:29:16 +0100

    cleanup

--- src/BestiaryArenaCracker.Domain/BoardItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BestiaryArenaCracker.Domain
-{
-    public class BoardItem
-    {
-        public required int Tile { get; set; }
-        public required Monster Monster { get; set; }
-        public required Item Equipment { get; set; }
-    }
-}
--- src/BestiaryArenaCracker.Domain/Composition.cs
+++ src/BestiaryArenaCracker.Domain/Composition.cs
@@ -1,28 +1,7 @@
-﻿using System.Security.Cryptography;
-using System.Text;
-using System.Text.Json;
-
-namespace BestiaryArenaCracker.Domain
+﻿namespace BestiaryArenaCracker.Domain
 {
     public class Composition
     {
-        public BoardItem[] Board { get; set; } = [];
 
-        private readonly JsonSerializerOptions options = new()
-        {
-            WriteIndented = false,
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
-        };
-
-        public string ComputeHash()
-        {
-            // Serialize the object
-            var json = JsonSerializer.Serialize(this, options);
-
-            // Compute SHA256 hash
-            var hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(json));
-            return Convert.ToHexString(hashBytes);
-        }
     }
 }
