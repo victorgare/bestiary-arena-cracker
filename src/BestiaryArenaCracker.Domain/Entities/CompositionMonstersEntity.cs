4445c7410f9f66302090755d46ef722e304c5753
Victor Carnielli <garevictor@hotmail.com>
2025-06-05 21:24:23 +0100

    default values for monsters

--- src/BestiaryArenaCracker.Domain/Entities/CompositionMonstersEntity.cs
+++ src/BestiaryArenaCracker.Domain/Entities/CompositionMonstersEntity.cs
@@ -4,15 +4,15 @@
     {
         public int CompositionId { get; set; }
         public string Name { get; set; } = null!;
-        public int Hitpoints { get; set; }
-        public int Attack { get; set; }
-        public int AbilityPower { get; set; }
-        public int Armor { get; set; }
-        public int MagicResistance { get; set; }
-        public int Level { get; set; }
+        public int Hitpoints { get; set; } = 20;
+        public int Attack { get; set; } = 20;
+        public int AbilityPower { get; set; } = 20;
+        public int Armor { get; set; } = 20;
+        public int MagicResistance { get; set; } = 20;
+        public int Level { get; set; } = 20;
         public int TileLocation { get; set; }
         public Equipments Equipment { get; set; }
         public EquipmentStat EquipmentStat { get; set; }
-        public int EquipmentTier { get; set; }
+        public int EquipmentTier { get; set; } = 5;
     }
 }
