a7601dd2b1f5225b4eaf903b8cc6a4f90683ca28
Victor Carnielli <garevictor@hotmail.com>
2025-06-11 21:34:26 +0100

    added rooms to skip on generation

--- src/BestiaryArenaCracker.ApplicationCore/Interfaces/Providers/IRoomConfigProvider.cs
+++ src/BestiaryArenaCracker.ApplicationCore/Interfaces/Providers/IRoomConfigProvider.cs
@@ -4,6 +4,11 @@ namespace BestiaryArenaCracker.ApplicationCore.Interfaces.Providers
 {
     public interface IRoomConfigProvider
     {
+        // rooms filtered by IgnoredRoomsIds, for generations purposes only
         IReadOnlyList<RoomConfig> Rooms { get; }
+
+        // list of all rooms, including those ignored by IgnoredRoomsIds
+        IReadOnlyList<RoomConfig> AllRooms { get; }
+
     }
 }
--- src/BestiaryArenaCracker.ApplicationCore/Providers/RoomConfigProvider.cs
+++ src/BestiaryArenaCracker.ApplicationCore/Providers/RoomConfigProvider.cs
@@ -1,6 +1,5 @@
 using BestiaryArenaCracker.ApplicationCore.Interfaces.Providers;
 using BestiaryArenaCracker.Domain.Room;
-using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -9,17 +8,25 @@ namespace BestiaryArenaCracker.ApplicationCore.Providers
     public class RoomConfigProvider : IRoomConfigProvider
     {
         public IReadOnlyList<RoomConfig> Rooms { get; }
+        public IReadOnlyList<RoomConfig> AllRooms { get; }
+
+        // ignore this rooms, for generations purposes only
+        public string[] IgnoredRoomsIds = ["rkswrs"];
 
         public RoomConfigProvider()
         {
             var basePath = AppContext.BaseDirectory;
             var jsonPath = Path.Combine(basePath, "data", "rooms_config.json");
             var json = System.IO.File.ReadAllText(jsonPath);
-            Rooms = JsonSerializer.Deserialize<List<RoomConfig>>(json, options: new()
+
+            var jsonConfig = JsonSerializer.Deserialize<List<RoomConfig>>(json, options: new()
             {
                 PropertyNameCaseInsensitive = true,
                 DefaultIgnoreCondition = JsonIgnoreCondition.Never
             }) ?? [];
+
+            Rooms = [.. jsonConfig.Where(c => !IgnoredRoomsIds.Contains(c.Id))];
+            AllRooms = jsonConfig;
         }
     }
 }
