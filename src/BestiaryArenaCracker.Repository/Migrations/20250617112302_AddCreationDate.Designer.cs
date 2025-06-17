94604cc0cf88454f9e4290981c7007ea299c33a5
Victor Carnielli <garevictor@hotmail.com>
2025-06-17 12:25:52 +0100

    feat: date created migration

--- src/BestiaryArenaCracker.Repository/Migrations/20250617112302_AddCreationDate.Designer.cs
+++ src/BestiaryArenaCracker.Repository/Migrations/20250617112302_AddCreationDate.Designer.cs
@@ -4,6 +4,7 @@ using BestiaryArenaCracker.Repository.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BestiaryArenaCracker.Repository.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250617112302_AddCreationDate")]
+    partial class AddCreationDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
